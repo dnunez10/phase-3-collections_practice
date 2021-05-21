2f3873cee92cafdc103ac84ec6d3f415f4f89bbc
dnunez10 <diana_nunez10@yahoo.com>
2021-05-20 18:37:42 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,53 @@ def sort_array_asc array
         a <=> b
     end
 end
+
+def sort_array_desc array
+    array.sort do |a, b|
+        if a == b
+            0
+        elsif a < b
+            1
+        elsif a > b
+            -1
+        end
+    end
+end
+
+def sort_array_char_count array
+    array.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements array
+    array.sort do |a, b|
+        a[1] <=> b[2]
+    end
+end
+
+def reverse_array array
+    array.reverse
+end
+
+def kesha_maker array
+    array.each do |c|
+        c[2] = "$"
+    end
+end
+
+def find_a array
+    array.select do |w|
+        w.start_with?("a")
+    end
+end
+
+def sum_array array
+    array.inject(:+)
+end
+
+def add_s array
+    array.each_with_index.collect do |w, index|
+        index == 1? w : w + "s"
+    end
+end
